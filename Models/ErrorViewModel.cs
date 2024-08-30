33c7885a9c8fe160b5c1fe12082a746f2b8424f1
Ghostcito <jmguticasani1254@gmail.com>
2024-08-30 17:56:45 -0500

    Primeros archivos

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Trabajo_Final.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
