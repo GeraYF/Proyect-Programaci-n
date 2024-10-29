d12447f78563772b1a70c2caabd2690c878f6011
Ghostcito <jmguticasani1254@gmail.com>
2024-10-28 20:25:57 -0500

    Logica Administradores

---
+++ ViewModel/AdminViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabajo_Final.ViewModel
+{
+    public class AdminViewModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+
+    }
+}
