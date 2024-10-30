b86fae7a140e7d1714ddbb6e46df3bfd9b71c5f2
GeraYF <geraldine_yanque@usmp.pe>
2024-10-30 01:03:53 -0500

    Models - historial de compra

---
+++ Models/Compra.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabajo_Final.Models
+{
+    public class Compra
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public decimal Total { get; set; }
+        public List<DetalleCompra>? Detalles { get; set; } // Relación con los detalles
+    }
+
+}
