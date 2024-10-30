58d33c70c0629c54b54744d871377cd84564f105
Ghostcito <jmguticasani1254@gmail.com>
2024-10-29 22:43:43 -0500

    Arreglando Login

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Trabajo_Final.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly ILogger<ProductosController> _logger;
+
+        public ProductosController(ILogger<ProductosController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
