03376bc319ef75f0c7c1b07dfa38b6e1fe0ec3f0
GeraYF <geraldine_yanque@usmp.pe>
2024-10-31 00:35:45 -0500

    Nuevo cambio

--- Migrations/20241031052741_AddCompraAndDetalleCompra.Designer.cs
+++ Migrations/20241031052741_AddCompraAndDetalleCompra.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Trabajo_Final.Data;
 
 #nullable disable
 
-namespace Trabajo_Final.Data.Migrations
+namespace Trabajo_Final.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241031052741_AddCompraAndDetalleCompra")]
+    partial class AddCompraAndDetalleCompra
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
