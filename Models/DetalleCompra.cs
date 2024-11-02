eb9ae7a088be67c3191173606b09d2dde290a42e
Ghostcito <jmguticasani1254@gmail.com>
2024-11-01 22:58:55 -0500

    Prueba

--- Models/DetalleCompra.cs
+++ Models/DetalleCompra.cs
@@ -6,11 +6,7 @@ namespace Trabajo_Final.Models
     public class DetalleCompra
     {
         public int Id { get; set; }
-        public int CompraId { get; set; }
-        [ForeignKey("CompraId")]
         public Compra Compra { get; set; }
-        public int ProductoId { get; set; }
-        [ForeignKey("ProductoId")]
         public Producto Producto { get; set; }
         public int Cantidad { get; set; }
         public decimal PrecioUnitario { get; set; }
