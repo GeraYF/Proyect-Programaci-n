9c151ee608174634325155ace708f58a3f32d8c1
Ghostcito <jmguticasani1254@gmail.com>
2024-11-01 23:10:44 -0500

    Arreglo

--- Models/Compra.cs
+++ Models/Compra.cs
@@ -9,12 +9,12 @@ namespace Trabajo_Final.Models
     [Table("t_compra")]
     public class Compra
     {
-        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public long ClienteId { get; set; }
         public DateTime FechaCompra { get; set; } = DateTime.UtcNow;
         public decimal Total { get; set; }
-       public IEnumerable<DetalleCompra> Detalles { get; set; } = new List<DetalleCompra>();
+        public IEnumerable<DetalleCompra> Detalles { get; set; } = new List<DetalleCompra>();
 
     }
 
--- Models/DetalleCompra.cs
+++ Models/DetalleCompra.cs
@@ -5,12 +5,13 @@ namespace Trabajo_Final.Models
         [Table("t_detalle_compra")]
         public class DetalleCompra
         {
-                public int Id { get; set; }
+                [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+                public long Id { get; set; }
                 public Compra Compra { get; set; }
                 public Producto Producto { get; set; }
-                public int Cantidad { get; set; }
-                public decimal PrecioUnitario { get; set; }
-                public decimal Precio { get; set; }
+                public int? Cantidad { get; set; }
+                public decimal? PrecioUnitario { get; set; }
+                public decimal? Precio { get; set; }
         }
 }
 
