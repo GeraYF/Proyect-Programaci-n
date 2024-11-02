f2cfd7a3b2f34f38218f931d845a3a84213d043d
andNiAl <andrea_alva2@usmp.pe>
2024-11-01 21:09:40 -0500

    modelo detalle compra modificado

--- Models/DetalleCompra.cs
+++ Models/DetalleCompra.cs
@@ -1,25 +1,20 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Trabajo_Final.Models
 {
-    
-    [Table("t_detalleCompra")]
-    
+    [Table("t_detalle_compra")]
     public class DetalleCompra
     {
         public int Id { get; set; }
         public int CompraId { get; set; }
+        [ForeignKey("CompraId")]
+        public Compra Compra { get; set; }
         public int ProductoId { get; set; }
-        public Producto? Producto { get; set; } 
-
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; }
         public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
         public decimal Precio { get; set; }
-
-        
     }
-
 }
+
