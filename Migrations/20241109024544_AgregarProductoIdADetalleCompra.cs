fac67946f51e73d1ebb51fe5fdb33b9b95466e3b
GeraYF <geraldine_yanque@usmp.pe>
2024-11-09 02:35:49 -0500

    cambios en los modelos

---
+++ Migrations/20241109024544_AgregarProductoIdADetalleCompra.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Trabajo_Final.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarProductoIdADetalleCompra : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "t_carrito");
+
+            migrationBuilder.DropTable(
+                name: "t_cliente");
+
+            migrationBuilder.DropTable(
+                name: "t_comentarios");
+
+            migrationBuilder.DropTable(
+                name: "t_contacto");
+
+            migrationBuilder.DropTable(
+                name: "t_detalle_compra");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "t_compra");
+
+            migrationBuilder.DropTable(
+                name: "t_producto");
+
+            migrationBuilder.DropTable(
+                name: "t_categoria");
+        }
+    }
+}
---
+++ Migrations/20241109034123_RenombrarCompraIdAIdCompramodificacion.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trabajo_Final.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenombrarCompraIdAIdCompramodificacion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_t_detalle_compra_t_compra_IdCompra",
+                table: "t_detalle_compra");
+
+            migrationBuilder.RenameColumn(
+                name: "IdCompra",
+                table: "t_detalle_compra",
+                newName: "CompraId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_t_detalle_compra_IdCompra",
+                table: "t_detalle_compra",
+                newName: "IX_t_detalle_compra_CompraId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_t_detalle_compra_t_compra_CompraId",
+                table: "t_detalle_compra",
+                column: "CompraId",
+                principalTable: "t_compra",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_t_detalle_compra_t_compra_CompraId",
+                table: "t_detalle_compra");
+
+            migrationBuilder.RenameColumn(
+                name: "CompraId",
+                table: "t_detalle_compra",
+                newName: "IdCompra");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_t_detalle_compra_CompraId",
+                table: "t_detalle_compra",
+                newName: "IX_t_detalle_compra_IdCompra");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_t_detalle_compra_t_compra_IdCompra",
+                table: "t_detalle_compra",
+                column: "IdCompra",
+                principalTable: "t_compra",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
